93918bf4c5ea6681ee9aff4bb1ecfd739c419fd3
Rafael Mafra <mafrazzrafael@gmail.com>
2017-05-14 09:37:46 -0300

    Added unit and arrive inputs & regex patter for coords

--- src/interface/queue/queue.js
+++ src/interface/queue/queue.js
@@ -3,6 +3,31 @@ define('FarmOverflow/QueueInterface', [
     'FarmOverflow/FrontButton'
 ], function (Interface, FrontButton) {
     return function (commandQueue) {
+        function genUnitsInput () {
+            let html = []
+
+            html.push('<tr>')
+
+            $model.getGameData().getOrderedUnitNames().forEach((unit, index) => {
+                if (index % 2 === 0 && index !== 0) {
+                    html.push('</tr><tr>')
+                }
+
+                let unitLabel = $filter('i18n')(unit, $root.loc.ale, 'unit_names')
+
+                html.push('<td class="cell-space-left">')
+                html.push(`<span class="float-left icon-bg-black icon-44x44-unit-${unit}"></span>`)
+                html.push('<div class="ff-cell-fix cell-space-44x44">')
+                html.push(`<span class="ng-binding">${unitLabel}</span><input class="unit" type="text" maxlength="5">`)
+                html.push('</div>')
+                html.push('</td>')
+            })
+
+            html.push('</tr>')
+
+            return html.join('')
+        }
+
         let queueButton = new FrontButton({
             label: 'Queue'
         })
@@ -13,7 +38,8 @@ define('FarmOverflow/QueueInterface', [
             htmlReplaces: {
                 version: commandQueue.version,
                 author: ___author,
-                title: 'CommandQueue'
+                title: 'CommandQueue',
+                unitsInput: genUnitsInput()
             }
         })
 
